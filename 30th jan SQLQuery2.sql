9009a602eb24aec6a21907671a86470f14e33965
smirti kumari <78090901+smriti121@users.noreply.github.com>
2025-02-01 14:24:31 +0530

    Add files via upload

---
+++ 30th jan SQLQuery2.sql
@@ -0,0 +1,103 @@
+use AdventureWorks2022;
+
+select * from HumanResources.Employee;
+
+select * from HumanResources.Employee where MaritalStatus = 'M';
+
+---find the employee who are working under marketing
+select * from HumanResources.Employee where JobTitle LIKE '%Marketing%';
+
+---find the employee who are working under Network Manager
+select * from HumanResources.Employee where JobTitle LIKE 'Network Manager';
+
+select count(*) from HumanResources.Employee;
+
+select count(*) from 
+HumanResources.Employee
+where Gender = 'M'; 
+
+select count(*) from 
+HumanResources.Employee
+where Gender = 'F'; 
+
+select count(MaritalStatus) from 
+HumanResources.Employee
+where Gender = 'F'; 
+
+---Find the employee having SalariedFlag as 1
+select * from HumanResources.Employee where SalariedFlag = 1;
+
+---Find the employee having VacationHours more than 70
+select * from HumanResources.Employee where VacationHours > 70;
+
+---VacationHour more than 70 but less than 90
+select * from HumanResources.Employee where VacationHours> 70 and VacationHours<90;
+
+---Find all jobs having title as Designer
+select * from HumanResources.Employee where JobTitle LIKE '%Designer%';
+
+---Find the total employee worked as Technician
+select count(*) from HumanResources.Employee where Jobtitle LIKE '%Technician%';
+
+---display the data having NationalIDNumber, JobvTitle,MaritalStatus, gender for all under marketting job titile 
+select NationalIDNumber, JobTitle, MaritalStatus, Gender from HumanResources.Employee where JobTitle LIKE '%Marketing%';
+
+---Find max vacation hours
+select Max(VacationHours) from HumanResources.Employee;
+
+---Find min sick leaves
+select Min(SickLeaveHours) from HumanResources.Employee;
+
+select * from HumanResources.Department where Name = 'Production';
+select * from HumanResources.Department where Name = '7';
+
+select * from HumanResources.Employee where BusinessEntityID in (select BusinessEntityID from HumanResources.EmployeeDepartmentHistory where DepartmentID = 7);
+
+
+select * from HumanResources.Department where GroupName = 'Reseach and Development';
+
+select * from HumanResources.Employee where BusinessEntityID in(select BusinessEntityID from HumanResources.EmployeeDepartmentHistory 
+where DepartmentID in 
+(select DepartmentID from HumanResources.Department where GroupName = 'Research and Development'))
+
+---find all emplolyees who work in day shift
+select * from HumanResources.Shift
+
+select * from HumanResources.Shift
+where Name ='Day'
+
+select * from HumanResources.EmployeeDepartmentHistory;
+
+select * from HumanResources.EmployeeDepartmentHistory
+where ShiftID = 1;
+
+select * from HumanResources.EmployeeDepartmentHistory
+where ShiftID = (select ShiftID from HumanResources.Shift
+where Name ='Day');
+
+select * from HumanResources.Employee where BusinessEntityID in(select BusinessEntityID from HumanResources.EmployeeDepartmentHistory 
+where ShiftID = (select ShiftID from HumanResources.Shift
+where Name ='Day'));
+
+---find all employees where pay frequency is 1  --pay
+select * from HumanResources.EmployeeDepartmentHistory where ShiftID =1;
+
+---find candidate who are not placed  
+
+select * from HumanResources.JobCandidate where BusinessEntityID in (select BusinessEntityID from HumanResources.Employee);
+
+---find the address of employee
+select * from Person.Address ;
+select AddressLine1, City, PostalCode from Person.Address where AddressID = (select AddressID from Person.BusinessEntityAddress where BusinessEntityID = 101);
+
+---find the name for employee working in group and development
+select FirstName, LastName from Person.Person where BusinessEntityID in (select BusinessEntityID from HumanResources.EmployeeDepartmentHistory where DepartmentID in (select DepartmentID from HumanResources.Department where GroupName='Research and Development'));
+
+
+
+
+
+
+
+
+
---
+++ 31st jan SQLQuery1.sql
@@ -0,0 +1,190 @@
+use AdventureWorks2022;
+
+select BusinessEntityId, NationalIdNumber, JobTitle,(select firstname from Person.Person p where p.BusinessEntityID = e.BusinessEntityID) FirstName from HumanResources.Employee e;
+
+---add personal details of employee middle name, last names
+
+select BusinessEntityID, NationalIDNumber, JobTitle,
+(select concat(firstname, ' ', middlename, ' ', lastname) from person.person p where p.BusinessEntityID = e.BusinessEntityID)FullName
+from HumanResources.Employee e;
+
+select BusinessEntityID, NationalIDNumber, JobTitle,
+(select concat_ws(' ',firstname, middlename, lastname) from person.person p where p.BusinessEntityID = e.BusinessEntityID)FullName
+from HumanResources.Employee e;
+
+---display national_id, first_name, last_name and department name, department group
+
+select NationalIDNumber from HumanResources.Employee;
+select * from  Person.Person;
+select * from HumanResources.EmployeeDepartmentHistory;
+select * from  HumanResources.Department;
+select BusinessEntityID, DepartmentID, ed.* from  HumanResources.EmployeeDepartmentHistory ed;
+
+select(select concat(firstname,lastname)
+from Person.Person p 
+where p.BusinessEntityID = ed.BusinessEntityID) Person_detail,
+(select NationalIDNumber from HumanResources.Employee e 
+where e.BusinessEntityID = ed.BusinessEntityID) EMP_detail,
+(select concat(name, GroupName) from HumanResources.Department d
+where d.DepartmentID = ed.DepartmentID)dept_details 
+from  HumanResources.EmployeeDepartmentHistory ed;
+
+
+
+---display first_name, last_name, department, ship time
+select
+    (select FirstName from Person.Person p where p.BusinessEntityID = ed.BusinessEntityID) AS FirstName,
+    (select LastName from Person.Person p where p.BusinessEntityID = ed.BusinessEntityID) AS LastName,
+    (select Name from HumanResources.Department d where d.DepartmentID = ed.DepartmentID) AS Department,
+    ed.ShiftID AS ShipTime
+from HumanResources.EmployeeDepartmentHistory ed;
+
+
+---display product name and product review based on production schema
+select 
+    p.Name AS ProductName, 
+    pr.ReviewerName AS ProductReview
+from Production.Product p
+JOIN Production.ProductReview pr
+    ON p.ProductID = pr.ProductID;
+
+
+--- find the employee's name, job title, credit card  expired in month 11 and year 2008
+select * from Person.Person;
+select * from HumanResources.Employee;
+select * from Sales.SalesOrderHeader;
+select * from Sales.CreditCard;
+
+select  
+    (select CONCAT(p.FirstName, ' ', p.LastName)  
+     from Person.Person p  
+     where p.BusinessEntityID = e.BusinessEntityID) AS EmployeeName,  
+
+    (select JobTitle  
+     from HumanResources.Employee  
+     where BusinessEntityID = e.BusinessEntityID) AS JobTitle,  
+
+    (select cc.CardNumber  
+     from Sales.CreditCard cc  
+     where cc.CreditCardID = soh.CreditCardID) AS CreditCardNumber  
+
+from Sales.SalesOrderHeader soh, HumanResources.Employee e  
+where soh.CreditCardID IN (  
+    select CreditCardID  
+    from Sales.CreditCard  
+    where ExpMonth = 11 AND ExpYear = 2008  
+)  
+AND soh.SalesPersonID = e.BusinessEntityID;
+
+
+
+
+
+--- display records from currency rate from USD to AUD
+select * from Sales.CurrencyRate;
+select *
+from Sales.CurrencyRate
+where FromCurrencyCode = 'USD' AND ToCurrencyCode = 'AUD';
+
+
+
+---display EMP name, territory name, group, SaleslastYear Salequota, bonus from germany and united kingdom
+select * from Sales.SalesTerritory;
+select 
+    (select CONCAT(firstName, ' ', lastName) 
+     from Person.Person p 
+     where p.BusinessEntityID = sp.BusinessEntityID) AS EmpName,
+    
+    (select Name 
+     from Sales.SalesTerritory st 
+     where st.TerritoryID = sp.TerritoryID) AS TerritoryName,
+    
+    (select [Group] 
+     from Sales.SalesTerritory st 
+     where st.TerritoryID = sp.TerritoryID) AS TerritoryGroup,
+
+    sp.SalesLastYear,
+    sp.SalesQuota,
+    sp.Bonus
+from Sales.SalesPerson sp
+where sp.TerritoryID IN (
+    select TerritoryID 
+    from Sales.SalesTerritory 
+    where Name IN ('Germany', 'United kingdom')
+);
+
+
+---find all employee who worked in all north America territory
+
+select 
+    (select CONCAT(firstName, ' ', lastName) 
+     from Person.Person p 
+     where p.BusinessEntityID = sp.BusinessEntityID) AS EmpName,
+    
+    (select Name 
+     from Sales.SalesTerritory st 
+     where st.TerritoryID = sp.TerritoryID) AS TerritoryName,
+    
+    (select [Group] 
+     from Sales.SalesTerritory st 
+     where st.TerritoryID = sp.TerritoryID) AS TerritoryGroup,
+
+    sp.SalesLastYear,
+    sp.SalesQuota,
+    sp.Bonus
+from Sales.SalesPerson sp
+where sp.TerritoryID IN (
+    select TerritoryID 
+    from Sales.SalesTerritory 
+    where [Group] = 'North America'
+);
+
+
+---find the product details in cart
+select * from Production.Product;
+select * from Sales.SalesOrderDetail;
+
+select 
+    (select p.Name 
+     from Production.Product p 
+     where p.ProductID = sod.ProductID) AS ProductName,
+    
+    sod.OrderQty AS Quantity,
+    sod.LineTotal AS TotalPrice,
+    
+    (select p.StandardCost 
+     from Production.Product p 
+     where p.ProductID = sod.ProductID) AS ProductCost,
+    
+    (select p.ListPrice 
+     from Production.Product p 
+     where p.ProductID = sod.ProductID) AS ProductPrice
+from Sales.SalesOrderDetail sod
+where sod.SalesOrderID = SalesOrderID; 
+
+---find the product with special offer
+select * from Sales.SpecialOffer;
+select * from Production.Product;
+
+select 
+    (select p.Name 
+     from Production.Product p 
+     where p.ProductID = sop.ProductID) AS ProductName,
+    
+    (select so.Description 
+     from Sales.SpecialOffer so 
+     where so.SpecialOfferID = sop.SpecialOfferID) AS SpecialOfferName,
+    
+    sop.SpecialOfferID,
+    sop.ProductID
+from Sales.SpecialOfferProduct sop
+where sop.SpecialOfferID IN (
+    select SpecialOfferID
+    from Sales.SpecialOffer
+    where StartDate <= GETDATE()  
+      AND EndDate >= GETDATE()    
+);
+
+select Name from Production.Product pp
+where pp.ProductID in(
+select ProductID from Sales.SpecialOffer where Type ='No Discount');
